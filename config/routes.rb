ed70fe06f0733133066c37795f23a364c9ade2dd
つづこ <>
2024-09-23 13:11:33 +0900

    top画面用のhomeを作成 rubocopの活用

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
-  get "/" => "devise/sessions#new"
+  root to: "home#index"
 
   # Render dynamic PWA files from app/views/pwa/*
   get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
